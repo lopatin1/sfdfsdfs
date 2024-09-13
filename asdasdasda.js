8dbc464f3bda263f9ae556eceb1d967fc052193e
Мансур <94774079+lopatin1@users.noreply.github.com>
2024-09-14 01:13:16 +0500

    Add files via upload

---
+++ asdasdasda.js
@@ -0,0 +1,212 @@
+const normal = document.getElementById("normal")
+const s1 = document.getElementById("s1")
+const s2 = document.getElementById("s2")
+const s3 = document.getElementById("s3")
+const s4 = document.getElementById("s4")
+const s5 = document.getElementById("s5")
+const s6 = document.getElementById("s6")
+const s7 = document.getElementById("s7")
+const s8 = document.getElementById("s8")
+const s9 = document.getElementById("s9")
+const s10 = document.getElementById("s10")
+const s11 = document.getElementById("s11")
+const s12 = document.getElementById("s12")
+const body = document.getElementById("s12")
+function clicker1(){
+    normal.className = "normal_n" ;
+    s1.className = "s1";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker2(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker3(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker4(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker5(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker6(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker7(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker8(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker9(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker10(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function clicker11(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11";
+    s12.className = "s12_n";
+}
+function clicker12(){
+    normal.className = "normal_n" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12";
+}
+function normal(){
+    normal.className = "normal" ;
+    s1.className = "s1_n";
+    s2.className = "s2_n";
+    s3.className = "s3_n";
+    s4.className = "s4_n";
+    s5.className = "s5_n";
+    s6.className = "s6_n";
+    s7.className = "s7_n";
+    s8.className = "s8_n";
+    s9.className = "s9_n";
+    s10.className = "s10_n";
+    s11.className = "s11_n";
+    s12.className = "s12_n";
+}
+function them(){
+
+}
